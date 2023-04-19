1a25f1e5ca3bec0542efd264878ed81fb5b79bc7
ertmaa18 <ertmaa18@htl-kaindorf.at>
2023-04-19 08:10:26 +0200

    init Spring

---
+++ src/main/java/at/kaindorf/ahif18/q3_rp_fileexplorer/Q3RpFileExplorerApplication.java
@@ -0,0 +1,13 @@
+package at.kaindorf.ahif18.q3_rp_fileexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q3RpFileExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Q3RpFileExplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/kaindorf/ahif18/q3_rp_fileexplorer/Q3RpFileExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.kaindorf.ahif18.q3_rp_fileexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q3RpFileExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
